88907dd53de00cfe7db5aaa3e0715b6da9252bd8
Wonseok <ls4050@naver.com>
2022-02-10 12:44:27 +0900

    Spring Boot Configuration

--- myapplication/src/main/java/ex01/MyApplication.java
+++ myapplication/src/main/java/ex01/MyApplication.java
@@ -1,10 +1,19 @@
 package ex01;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+//@Configuration
+@SpringBootConfiguration
 public class MyApplication {
-
+	
+	@Bean
+	public MyComponent myComponent() {
+		return new MyComponent();
+	}
+	
 	public static void main(String[] args) {
 		try (ConfigurableApplicationContext c = SpringApplication.run(MyApplication.class, args)) {
 			
