dfd118135a0f2f62ea8c9f5c0b694003f01ffa37
Wonseok <ls4050@naver.com>
2022-02-27 20:41:57 +0900

    #spring boot starter build

---
+++ dependency/src/main/java/com/poscoict/JDBCConnectionManagerRunner.java
@@ -0,0 +1,20 @@
+package com.poscoict;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+import com.poscoict.jdbc.util.JDBCConnectionManager;
+
+@Service
+public class JDBCConnectionManagerRunner implements ApplicationRunner{
+	
+	@Autowired
+	private JDBCConnectionManager connectionManager;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("커넥션 매니저 : " + connectionManager.toString());
+	}
+}
