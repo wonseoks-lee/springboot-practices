17297ab7d599413049cce40bf33c5ece78f94170
Wonseok <ls4050@naver.com>
2022-03-13 02:41:32 +0900

    jpa prac

---
+++ jpa-h2-prac/src/main/java/com/poscoict/JPAClient.java
@@ -0,0 +1,47 @@
+package com.poscoict;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.poscoict.domain.Board;
+
+public class JPAClient {
+	public static void main(String[] args) {
+		// EntityManager 생성
+		EntityManagerFactory emf = 
+				Persistence.createEntityManagerFactory("jpa-h2-prac");
+		EntityManager em = emf.createEntityManager();
+		
+		
+		/// Transaction 생성
+		EntityTransaction tx = em.getTransaction();
+		try {
+			// Transaction 시작
+			tx.begin();
+			
+			Board board = new Board();
+			board.setTitle("JPA제목");
+			board.setWriter("관리자");
+			board.setContent("JPA 글 등록 잘 되네요.");
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+			
+			// 글 등록
+			em.persist(board);
+			
+			// Transaction commit
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			em.close();
+			emf.close();
+		}
+		
+	}
+}
