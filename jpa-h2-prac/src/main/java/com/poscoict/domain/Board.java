63e4b2039b683a9169b60794b3c9cd70c1d91024
Wonseok <ls4050@naver.com>
2022-03-13 23:10:23 +0900

    JPA prac #AutoGenerator

--- jpa-h2-prac/src/main/java/com/poscoict/domain/Board.java
+++ jpa-h2-prac/src/main/java/com/poscoict/domain/Board.java
@@ -19,15 +19,10 @@ import javax.persistence.TemporalType;
  */
 
 @Entity
-@SequenceGenerator(name= "BOARD_SEQ_GENERATOR",
-				sequenceName = "BOARD_SEQUENCE",
-				initialValue = 1,
-				allocationSize = 1)
 public class Board{
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-					generator = "BOARD_SEQ_GENERATOR")
+	@GeneratedValue
 	private Long seq;
 	
 	private String title;
