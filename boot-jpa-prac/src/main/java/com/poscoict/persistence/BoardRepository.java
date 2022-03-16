b2836227ab24bbba493672d378a7f86ceddbf991
Wonseok <ls4050@naver.com>
2022-03-17 01:20:35 +0900

    JPA like, find

--- boot-jpa-prac/src/main/java/com/poscoict/persistence/BoardRepository.java
+++ boot-jpa-prac/src/main/java/com/poscoict/persistence/BoardRepository.java
@@ -1,9 +1,13 @@
 package com.poscoict.persistence;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.poscoict.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long>{
-	
+	List<Board> findByTitle(String searchKeyword);
+	List<Board> findByContentContainingOrderBySeqDesc(String searchKeyword);
+	List<Board> findByTitleContainingOrContentContaining(String title, String content);
 }
