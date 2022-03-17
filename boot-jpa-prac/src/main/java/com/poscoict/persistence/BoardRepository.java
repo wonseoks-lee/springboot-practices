efd87501d2ef0e5454dd36a44251c73fe535149e
Wonseok <ls4050@naver.com>
2022-03-18 01:39:05 +0900

    nativeQuery

--- boot-jpa-prac/src/main/java/com/poscoict/persistence/BoardRepository.java
+++ boot-jpa-prac/src/main/java/com/poscoict/persistence/BoardRepository.java
@@ -33,8 +33,12 @@ public interface BoardRepository extends CrudRepository<Board, Long>{
 	@Query("SELECT b from Board b WHERE b.title like %:searchKeyword% ORDER BY b.seq DESC")
 	List<Board> queryAnnotationTest1(@Param("searchKeyword") String searchKeyword);
 	
+	// 특정 변수값들을 조회할때 object[]타입을 사용해야함
 	@Query("SELECT b.seq, b.title, b.writer, b.createDate FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
-	List<Object[]> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
+	List<Object[]> queryAnnotationTest2(String searchKeyword);
 	
+	//persisstance를 뒤져본것이아닌 db를 뒤져본 
+	@Query(value="select seq, title, writer, create_date from board where title like %?1% order by seq desc", nativeQuery=true)
+	List<Object[]> queryAnnotationTest3(String searchKeyword);
 	
 }
--- boot-jpa-prac/src/test/java/com/poscoict/QueryAnnotationTest.java
+++ boot-jpa-prac/src/test/java/com/poscoict/QueryAnnotationTest.java
@@ -1,5 +1,6 @@
 package com.poscoict;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.poscoict.domain.Board;
 import com.poscoict.persistence.BoardRepository;
 
 @RunWith(SpringRunner.class)
@@ -17,13 +17,33 @@ public class QueryAnnotationTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
+//	@Test
+//	public void testQueryAnnotationTest1() {
+//		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목 10");
+//		
+//		System.out.println("검색 결과");
+//		for(Board board : boardList) {
+//			System.out.println("---> "+ board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testQueryAnnotationTest2() {
+//		List<Object[]> boardList = boardRepo.queryAnnotationTest2("테스트 제목 10");
+//		
+//		System.out.println("검색 결과");
+//		for(Object[] row : boardList) {
+//			System.out.println("---> "+ Arrays.toString(row));
+//		}
+//	}
+	
 	@Test
-	public void testQueryAnnotationTest1() {
-		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목 10");
+	public void testQueryAnnotationTest3() {
+		List<Object[]> boardList = boardRepo.queryAnnotationTest3("테스트 제목 10");
 		
 		System.out.println("검색 결과");
-		for(Board board : boardList) {
-			System.out.println("---> "+ board.toString());
+		for(Object[] row : boardList) {
+			System.out.println("---> "+ Arrays.toString(row));
 		}
 	}
 }
