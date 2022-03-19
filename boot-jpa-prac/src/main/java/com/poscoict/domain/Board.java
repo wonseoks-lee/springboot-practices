a734fe79d0632b0630c3c391997cb19e7ba7b9bc
Wonseok <ls4050@naver.com>
2022-03-19 18:19:27 +0900

    outter join -> inner join

--- boot-jpa-prac/src/main/java/com/poscoict/domain/Board.java
+++ boot-jpa-prac/src/main/java/com/poscoict/domain/Board.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -22,9 +24,13 @@ public class Board{
 	private Long seq;
 	
 	private String title;
-	private String writer;
+//	private String writer;
 	private String content;
 	@Temporal(value = TemporalType.TIMESTAMP)
 	private Date createDate;
 	private Long cnt;
+	
+	@ManyToOne
+	@JoinColumn(name="MEMBER_ID", nullable=false)
+	private Member member;
 }
